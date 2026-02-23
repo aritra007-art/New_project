8ae309537d85cab8c896c3b69bd9fb8ee9587823
Aritra Pal <aritra345pal@gmail.com>
2026-02-23 16:03:02 +0530

    Add files via upload

---
+++ Deletion of an Element from a particular position in 1D array - C language.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+   int n,a[n],i,pos;
+   printf("Enter range\n");
+   scanf("%d",&n);
+   printf("Enter elements to 1D array\n");
+   for(i=0;i<n;i++) {
+       scanf("%d",&a[i]);
+   }
+   
+   printf("Enter the position from where element is to be deleted\n");
+   scanf("%d",&pos);
+   
+   for(i=pos;i<n-1;i++) {
+       a[i]=a[i+1];
+   }
+   
+   for(i=0;i<n-1;i++)   {
+       printf("%d\t",a[i]);
+   }
+  
+    return 0;
+}
